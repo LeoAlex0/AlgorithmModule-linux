dc5e42967ed3bc399a1ba9030be8b400b71a0d66
Grant Howard <the1812@outlook.com>
2017-12-22 20:20:06 +0800

    C适配：输出部分。（未测试）

--- src/algorithm_module/c_adapt.cpp
+++ src/algorithm_module/c_adapt.cpp
@@ -6,12 +6,31 @@ namespace GraphAlgorithm
     typedef int (*_NextEdgeAdapter)(int idx_v, int idx_e);
     typedef int (*_EdgeToAdapter)(int idx_v, int idx_e);
     PathType bfsResult;
+    std::vector<int> pointVector, edgeVector;
     C_PUBLIC void RunBfs(int idx_start_v, int idx_dest_v,
                        _FirstEdgeAdapter firstOf, 
                        _NextEdgeAdapter nextOf,
                        _EdgeToAdapter destOf)
     {
         bfsResult = bfs(idx_start_v, idx_dest_v, FirstEdgeAdapter(firstOf), NextEdgeAdapter(nextOf), EdgeToAdapter(destOf));
-    };
+        pointVector = std::vector<int>(bfsResult.first.begin(), bfsResult.first.end());
+        edgeVector = std::vector<int>(bfsResult.second.begin(), bfsResult.second.end());
 
+    };
+    C_PUBLIC int* GetPointListPointer()
+    {
+        return &pointVector[0];
+    }
+    C_PUBLIC int GetPointListLength()
+    {
+        return pointVector.size();
+    }
+    C_PUBLIC int* GetEdgeListPointer()
+    {
+        return &edgeVector[0];
+    }
+    C_PUBLIC int GetEdgeListLength()
+    {
+        return edgeVector.size();
+    }
 }
