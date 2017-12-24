f08ba438825e2e75684ebd2f26ede49f72ecdc1e
Leo Alex <z.alex.leo@outlook.com>
2017-12-24 23:48:20 +0800

    [未测试]修正一些显而易见的BUG，修改缩进模式为VS2017默认,添加最为基础的c_adapter

--- src/algorithm_module/c_adapt.cpp
+++ src/algorithm_module/c_adapt.cpp
@@ -1,8 +1,32 @@
 #include "algorithm_module.h"
-#include "c_algorithm_module"
+#include "c_algorithm_module.h"
+
+AM_PathType PathType_cast(const Algorithm::Graph::PathType& a) {
+	if (a == Algorithm::Graph::NIL_PATH)
+		return AM_NIL_PATH;
+
+	AM_PathType ret;
+	ret.vertexSize = a.first.size();
+	ret.vertexList = (int*)malloc(sizeof(int)*ret.vertexSize);
+	int idx = 0;
+	for (int v : a.first)
+		ret.vertexList[idx++] = v;
+
+	ret.edgeSize = a.second.size();
+	ret.edgeList = (int*)malloc(sizeof(int)*ret.edgeSize);
+	idx = 0;
+	for (int e : a.second)
+		ret.edgeList[idx++] = e;
+	return ret;
+}
 
 C_PUBLIC AM_PathType
-AM_bfs(int idx_start_v,int idx_dest_v,
-       AM_FirstEdgeAdapter firstOf,AM_NextEdgeAdapter nextOf
-       AM_EdgeToAdapter destOf){
+AM_bfs(
+	int idx_start_v,int idx_dest_v,
+	AM_FirstEdgeAdapter firstOf,AM_NextEdgeAdapter nextOf,
+	AM_EdgeToAdapter destOf){
+
+	return PathType_cast(Algorithm::Graph::bfs(
+		idx_start_v, idx_dest_v,
+		firstOf, nextOf, destOf));
 }
